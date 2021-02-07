5e79c53f6635b94ccd287cc1fd4ff231187d5edc
MhmdRyhn <rayhan.cse12@gmail.com>
2021-02-07 12:05:50 +0600

    Update user_data.sh

--- terraform/templates/user_data.sh
+++ terraform/templates/user_data.sh
@@ -3,4 +3,15 @@
 set -e
 set -x
 
-echo "Auto Scaling Group"
+cd /var//www/
+# In real, SSH url with public key authentication should be used.
+# Don't use username and password to clone the repo.
+git clone https://github.com/MhmdRyhn/aws-auto-scaling.git
+cd aws-auto-scaling
+pip3 install virtualenv
+# create vurtual environment
+virtualenv -p python3 venv
+venv/bin/pip install -r requirements.txt
+cp autoscale.apache2.conf /etc/apache2/sites-available/autoscale.apache2.conf
+sudo a2ensite flask-demo.apache2.conf
+sudo systemctl reload apache2
