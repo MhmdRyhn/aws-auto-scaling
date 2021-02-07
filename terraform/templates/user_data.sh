b4b4e5dd3c58be1fe16bc4eebd76d09877782a97
MhmdRyhn <rayhan.cse12@gmail.com>
2021-02-08 05:08:56 +0600

    Fix user_data.sh by adding permission and adding more required commands

--- terraform/templates/user_data.sh
+++ terraform/templates/user_data.sh
@@ -7,10 +7,12 @@ cd /var//www/
 # In real, SSH url with public key authentication should be used.
 # Don't use username and password to clone the repo.
 git clone https://github.com/MhmdRyhn/aws-auto-scaling.git
+sudo chmod -R 757 aws-auto-scaling/
 cd aws-auto-scaling
-# create vurtual environment
 virtualenv -p python3 venv
 venv/bin/pip install -r requirements.txt
-cp autoscale.apache2.conf /etc/apache2/sites-available/autoscale.apache2.conf
-sudo a2ensite flask-demo.apache2.conf
+sudo cp autoscale.apache2.conf /etc/apache2/sites-available/autoscale.apache2.conf
+sudo a2ensite autoscale.apache2.conf
+# Disable default configuration
+sudo a2dissite 000-default.conf
 sudo systemctl reload apache2
