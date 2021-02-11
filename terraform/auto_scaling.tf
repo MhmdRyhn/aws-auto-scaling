2916f259005b81553e445a32d76f7bfed35ad62d
MhmdRyhn <rayhan.cse12@gmail.com>
2021-02-11 20:32:44 +0600

    Fix scale down policy with SimpleScaling policy type

--- terraform/auto_scaling.tf
+++ terraform/auto_scaling.tf
@@ -36,35 +36,37 @@ resource "aws_autoscaling_attachment" "autoscaling_target_group_attachment" {
 
 
 resource "aws_autoscaling_policy" "scale_out_policy" {
+  # It explains the scaling policy types --> `SimpleScaling`, `StepScaling` and `TargetTrackingScaling`.
+  # https://acloud.guru/forums/aws-certified-solutions-architect-associate/discussion/-LTMH8An16Q-Airb61mo/difference-below-auto-scaling-policies?answer=-LTNp1ecDGvkBpVvRjbP
   name                    = "${local.resource_name_prefix}-scale-out-policy"
   autoscaling_group_name  = aws_autoscaling_group.auto_scaling_group.name
   adjustment_type         = "ExactCapacity"
   policy_type             = "StepScaling"
   metric_aggregation_type = "Sum"
   step_adjustment {
-    scaling_adjustment          = 1
+    scaling_adjustment          = 2
     # Upper/Lower bound is --> [Metric value - Breach Threshold]
     # Ref: https://docs.aws.amazon.com/autoscaling/ec2/APIReference/API_StepAdjustment.html
     metric_interval_lower_bound = 0
     metric_interval_upper_bound = var.request_per_server
   }
   step_adjustment {
-    scaling_adjustment          = 2
+    scaling_adjustment          = 3
     metric_interval_lower_bound = var.request_per_server
     metric_interval_upper_bound = var.request_per_server * 2
   }
   step_adjustment {
-    scaling_adjustment          = 3
+    scaling_adjustment          = 4
     metric_interval_lower_bound = var.request_per_server * 2
     metric_interval_upper_bound = var.request_per_server * 3
   }
   step_adjustment {
-    scaling_adjustment          = 4
+    scaling_adjustment          = 5
     metric_interval_lower_bound = var.request_per_server * 3
     metric_interval_upper_bound = var.request_per_server * 4
   }
   step_adjustment {
-    scaling_adjustment          = 5
+    scaling_adjustment          = 6
     metric_interval_lower_bound = var.request_per_server * 4
   }
 
@@ -80,29 +82,12 @@ resource "aws_autoscaling_policy" "scale_out_policy" {
 
 
 resource "aws_autoscaling_policy" "scale_in_policy" {
-  name                    = "${local.resource_name_prefix}-scale-in-policy"
-  autoscaling_group_name  = aws_autoscaling_group.auto_scaling_group.name
-  adjustment_type         = "ExactCapacity"
-  policy_type             = "StepScaling"
+  # It explains the scaling policy types --> `SimpleScaling`, `StepScaling` and `TargetTrackingScaling`.
+  # https://acloud.guru/forums/aws-certified-solutions-architect-associate/discussion/-LTMH8An16Q-Airb61mo/difference-below-auto-scaling-policies?answer=-LTNp1ecDGvkBpVvRjbP
+  name = "${local.resource_name_prefix}-scale-in-policy"
+  autoscaling_group_name = aws_autoscaling_group.auto_scaling_group.name
+  adjustment_type = "ExactCapacity"
+  policy_type = "SimpleScaling"
   metric_aggregation_type = "Sum"
-
-  step_adjustment {
-    scaling_adjustment          = 1
-//    metric_interval_lower_bound = - var.request_per_server * 2
-    metric_interval_upper_bound = 0
-  }
-//  step_adjustment {
-//    scaling_adjustment          = -2
-//    metric_interval_lower_bound = - var.request_per_server * 2
-//    metric_interval_upper_bound = - var.request_per_server * 3
-//  }
-//  step_adjustment {
-//    scaling_adjustment          = -3
-//    metric_interval_lower_bound = - var.request_per_server * 4
-//    metric_interval_upper_bound = - var.request_per_server * 3
-//  }
-//  step_adjustment {
-//    scaling_adjustment          = - 4
-//    metric_interval_upper_bound = - var.request_per_server * 4
-//  }
+  scaling_adjustment = 1
 }
--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -4,7 +4,6 @@ resource "aws_cloudwatch_metric_alarm" "request_count_per_target_scale_out_alarm
   namespace         = "AWS/ApplicationELB"
   dimensions = {
     TargetGroup = aws_lb_target_group.primary_alb_target_group.arn_suffix
-//    LoadBalancer = aws_lb.primary_alb.arn
   }
   metric_name         = "RequestCountPerTarget"
   statistic           = "Sum"
@@ -17,19 +16,19 @@ resource "aws_cloudwatch_metric_alarm" "request_count_per_target_scale_out_alarm
 }
 
 
-//resource "aws_cloudwatch_metric_alarm" "request_count_per_target_scale_in_alarm" {
-//  alarm_name        = "${local.resource_name_prefix}-request-count-per-target-scale-in-alarm"
-//  alarm_description = "Scale out alarm when the number-of-request-count <= threshold"
-//  namespace         = "AWS/ApplicationELB"
-//  dimensions = {
-//    TargetGroup = aws_lb_target_group.primary_alb_target_group.arn_suffix
-//  }
-//  metric_name         = "RequestCountPerTarget"
-//  statistic           = "Sum"
-//  comparison_operator = "LessThanOrEqualToThreshold"
-//  threshold           = var.request_per_server
-//  period              = var.period
-//  evaluation_periods  = var.datapoints
-//  datapoints_to_alarm = var.datapoints_to_alarm
-//  alarm_actions       = [aws_autoscaling_policy.scale_in_policy.arn]
-//}
+resource "aws_cloudwatch_metric_alarm" "request_count_per_target_scale_in_alarm" {
+  alarm_name        = "${local.resource_name_prefix}-request-count-per-target-scale-in-alarm"
+  alarm_description = "Scale out alarm when the number-of-request-count <= threshold"
+  namespace         = "AWS/ApplicationELB"
+  dimensions = {
+    TargetGroup = aws_lb_target_group.primary_alb_target_group.arn_suffix
+  }
+  metric_name         = "RequestCountPerTarget"
+  statistic           = "Sum"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  threshold           = var.request_per_server
+  period              = var.period
+  evaluation_periods  = var.datapoints
+  datapoints_to_alarm = var.datapoints_to_alarm
+  alarm_actions       = [aws_autoscaling_policy.scale_in_policy.arn]
+}
